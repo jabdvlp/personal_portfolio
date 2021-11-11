cf892ae03841ba9a8c95d01d6bf45e572bf5ee09
JabDvlp <jacacio.89@gmail.com>
2021-11-11 16:44:21 -0400

    [ADD] Adding models and controllers

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +18,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+/* Route::get('/login', function(){
+    return view('home');
+}); */
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
